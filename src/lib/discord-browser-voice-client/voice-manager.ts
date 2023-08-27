e1bbebafdf3d78c32125b5c97266a7ecaa5a47b9
Rex <kieran@taggit.com>
2023-08-27 23:35:05 +0200

    kinna works when moving channels now

--- src/lib/discord-browser-voice-client/voice-manager.ts
+++ src/lib/discord-browser-voice-client/voice-manager.ts
@@ -1,15 +1,15 @@
 import EventEmitter from 'eventemitter3'
 import { GatewayDispatchEvents, GatewayOpcodes } from 'discord-api-types/v10'
 import type { GatewaySocket } from './gateway-socket'
-import { VoiceSocket, type Codecs } from './voice-socket'
+import {
+	VoiceSocket,
+	type Codecs,
+	type VoiceStateUpdate,
+	type VoiceServerUpdate
+} from './voice-socket'
 import { VoiceRTC, type AudioSettings } from './voice-rtc'
 import { VoiceOpcodes } from 'discord-api-types/voice'
 import { VoiceConnectionError, VoiceSpeakingError } from './errors'
-
-export enum VoiceState {
-	DISCONNECTED,
-	CONNECTED
-}
 
 export type ConnectionParams = {
 	guild_id: string
@@ -21,7 +21,6 @@ export type ConnectionParams = {
 		self_deaf?: boolean
 	} & AudioSettings
 }
-
 export interface VoiceManager extends EventEmitter {
 	on(event: 'track', listener: (event: MediaStreamTrack) => void): this
 	on(event: 'connected', listener: () => void): this
@@ -35,27 +34,30 @@ export interface VoiceManager extends EventEmitter {
 }
 
 export class VoiceManager extends EventEmitter {
-	private guild_id?: string
+	private guild_id: string | null = null
+	private channel_id: string | null = null
+	private self_mute = false
+	private self_deaf = false
+
 	private gateway: GatewaySocket
 	private voice?: VoiceSocket
 	private rtc?: VoiceRTC
-	private ssrc?: number
+
 	private track?: MediaStreamTrack
+	private ssrc?: number
+
+	private audio_settings?: AudioSettings
 	private initial_speaking = false
-	private audioSettings?: AudioSettings
 	private debug = false
-
-	private _state = VoiceState.DISCONNECTED
-
-	public get state() {
-		return this._state
-	}
 
 	constructor(params: { gatewaySocket: GatewaySocket; debug?: boolean }) {
 		super()
 
 		const { gatewaySocket, debug } = params
+
 		this.gateway = gatewaySocket
+		this.gateway.on('packet', (p) => this.handleGatewayPacket(p))
+
 		this.debug = debug ?? false
 	}
 
@@ -65,31 +67,53 @@ export class VoiceManager extends EventEmitter {
 		const { guild_id, channel_id, audio_track, audio_settings } = params
 
 		this.guild_id = guild_id
-		this.initial_speaking = audio_settings?.initial_speaking ?? false
-		this.track = audio_track
-		this.audioSettings = audio_settings
+		this.channel_id = channel_id
+		this.self_mute = audio_settings?.self_mute ?? false
+		this.self_deaf = audio_settings?.self_deaf ?? false
 
-		this.gateway.on('packet', (p) => this.handleGatewayPacket(p))
+		this.track = audio_track ?? this.track
+		this.audio_settings = audio_settings ?? this.audio_settings
+		this.initial_speaking = audio_settings?.initial_speaking ?? false
+
 		this.gateway.sendPacket({
 			op: GatewayOpcodes.VoiceStateUpdate,
 			d: {
 				guild_id,
 				channel_id,
-				self_mute: audio_settings?.self_mute ?? false,
-				self_deaf: audio_settings?.self_deaf ?? false
+				self_mute: this.self_mute,
+				self_deaf: this.self_deaf
 			}
 		})
 	}
 
 	private async handleGatewayPacket(packet: any) {
 		switch (packet.t) {
+			case GatewayDispatchEvents.VoiceStateUpdate: {
+				const { channel_id, user_id } = packet.d as VoiceStateUpdate['d']
+
+				if (user_id !== this.gateway.identity.id) return
+
+				this.channel_id === channel_id
+
+				if (!channel_id) {
+					this.rtc?.destroy()
+					this.voice?.destroy(true)
+					return
+				}
+
+				break
+			}
+
 			case GatewayDispatchEvents.VoiceServerUpdate: {
-				const { endpoint, guild_id, token } = packet.d
+				const { endpoint, guild_id, token } = packet.d as VoiceServerUpdate['d']
+
+				this.rtc?.destroy()
+				this.voice?.destroy()
 
 				this.rtc = new VoiceRTC({ debug: this.debug })
 				this.rtc.on('track', (t) => this.emit('track', t))
 
-				await this.rtc.openConnection(this.track!, this.audioSettings)
+				await this.rtc.openConnection(this.track!, this.audio_settings)
 				const { sdp, codecs, ssrc } = await this.rtc.createOffer()
 
 				this.ssrc = ssrc
@@ -103,7 +127,10 @@ export class VoiceManager extends EventEmitter {
 					debug: this.debug
 				})
 				this.voice.on('packet', (p) => this.handleVoicePacket(p, sdp, codecs))
-				this.voice.on('close', () => this.emit('disconnected'))
+				this.voice.on('close', () => {
+					this.rtc?.destroy()
+					this.emit('disconnected')
+				})
 			}
 		}
 	}
@@ -119,7 +146,6 @@ export class VoiceManager extends EventEmitter {
 				await this.rtc!.handleAnswer(packet.d.sdp)
 				this.setSpeaking(this.initial_speaking)
 				this.emit('connected')
-				this._state = VoiceState.CONNECTED
 				break
 			}
 		}
@@ -138,15 +164,33 @@ export class VoiceManager extends EventEmitter {
 		})
 	}
 
+	public move(params: {
+		guild_id: string
+		channel_id: string
+		self_mute?: boolean
+		self_deaf?: boolean
+	}) {
+		const { guild_id, channel_id, self_deaf, self_mute } = params
+
+		this.gateway.sendPacket({
+			op: GatewayOpcodes.VoiceStateUpdate,
+			d: {
+				guild_id,
+				channel_id,
+				self_mute: self_mute ?? this.self_mute,
+				self_deaf: self_deaf ?? this.self_deaf
+			}
+		})
+	}
+
 	public disconnect() {
-		this._state = VoiceState.DISCONNECTED
 		this.gateway.sendPacket({
 			op: GatewayOpcodes.VoiceStateUpdate,
 			d: {
 				guild_id: this.guild_id,
 				channel_id: null,
-				self_mute: false,
-				self_deaf: false
+				self_mute: this.self_mute,
+				self_deaf: this.self_deaf
 			}
 		})
 		this.rtc?.destroy()
