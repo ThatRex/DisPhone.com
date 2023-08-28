17ecadb45835e685dc5352ec22a6072dd7e1d8e7
Rex <rex@rexslab.com>
2023-08-28 02:04:04 +0200

    updated ui

--- src/routes/bot/+page.svelte
+++ src/routes/bot/+page.svelte
@@ -1,40 +1,55 @@
 <script lang="ts">
 	import { playAudioFromUrls } from '$lib/utils/play-audio-from-urls'
-	import Bot from '$lib/discord-browser-voice-client'
-	import { env } from '$env/dynamic/private'
+	import VoiceBot from '$lib/discord-browser-voice-client'
+	import { env } from '$env/dynamic/public'
 	import { dev } from '$app/environment'
+	import type { VoiceManager } from '$lib/discord-browser-voice-client/voice-manager'
 
-	export async function init() {
-		const bot = new Bot({ token: env.DISCORD_TOKEN, debug: dev })
+	let bot: VoiceBot
+	let ready = false
+	let voice: VoiceManager | undefined
+	let connected = false
 
-		bot.on('ready', async () => {
-			const stream_ = await navigator.mediaDevices.getUserMedia({ audio: true })
-			const stream = await playAudioFromUrls([
-				'03. Shades Of Hell - Kuolleet Sielut.mp3',
-				'ss_my_new_christ.mp3',
-				'test.mp3'
-			])
+	// https://cdn.discordapp.com/avatars/438008175698903040/05ce96076a76ee389254283dd160232d.webp?size=24
 
-			const audio_track = stream.getTracks()[0]
+	function init() {
+		bot = new VoiceBot({ token: env.PUBLIC_DISCORD_TOKEN, debug: dev })
+		bot.on('ready', () => (ready = true))
+	}
 
-			const voice = bot.connect({
+	async function connect() {
+		const stream_ = await navigator.mediaDevices.getUserMedia({ audio: true })
+		const stream = await playAudioFromUrls(
+			['test.mp3', 'ss_my_new_christ.mp3', '03. Shades Of Hell - Kuolleet Sielut.mp3'].sort(
+				() => Math.random() - 0.5
+			)
+		)
+
+		const audio_track = stream.getTracks()[0]
+
+		if (!voice) {
+			voice = bot.connect({
 				audio_track,
 				guild_id: '559178010838958090',
 				channel_id: '1035954430975168532',
+				self_deaf: true,
+				initial_speaking: true,
 				audio_settings: {
 					bitrate_kbps: 320,
 					mode: 'sendonly',
-					stereo: true,
-					self_deaf: true,
-					initial_speaking: true
+					stereo: true
 				}
 			})
 
 			voice.on('connected', () => {
-				setTimeout(() => voice.disconnect(), 2000)
+				connected = true
 				console.log('Connected')
 			})
-			voice.on('disconnected', () => console.log('Disconnected'))
+			voice.on('disconnected', () => {
+				connected = false
+				console.log('Disconnected')
+			})
+
 			voice.on('track', async (t) => {
 				const stream = new MediaStream()
 				const mediaElement = new Audio()
@@ -42,8 +57,41 @@
 				mediaElement.srcObject = stream
 				await mediaElement.play()
 			})
-		})
+		} else {
+			voice.connect({
+				guild_id: '559178010838958090',
+				channel_id: '1035954430975168532'
+			})
+		}
 	}
 </script>
 
-<button on:click={() => init()} />
+{#if !ready}
+	<button on:click={() => init()}>Start</button>
+{:else}
+	<button
+		on:click={() => {
+			voice = undefined
+			bot.shutdown()
+			ready = false
+		}}
+	>
+		Stop
+	</button>
+	<div>
+		{#if connected}
+			<button on:click={() => voice?.disconnect()}>Disconnect</button>
+			<button
+				on:click={() =>
+					voice?.move({
+						guild_id: '559178010838958090',
+						channel_id: '559178010838958095'
+					})}
+			>
+				Move
+			</button>
+		{:else}
+			<button on:click={async () => await connect()}>Connect</button>
+		{/if}
+	</div>
+{/if}
