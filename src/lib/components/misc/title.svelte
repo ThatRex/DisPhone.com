2ca3acb41230d30ceead7c266434445016e0c3cf
Rex <48845898+ThatRex@users.noreply.github.com>
2023-10-21 00:57:36 +0200

    Indev (#3)
    * updated bot exports
    
    * added transfer; fixed ring.wav request loops

---
+++ src/lib/components/misc/title.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { dev } from '$app/environment'
+
+	export let title: string | string[] = ''
+
+	$: title_list = ['BaitKit.net', ...(typeof title === 'string' ? [title] : title)].filter((i) => i)
+</script>
+
+<svelte:head>
+	<title>{dev ? '[InDev] ' : ''}{title_list.reverse().join(' • ')}</title>
+</svelte:head>
