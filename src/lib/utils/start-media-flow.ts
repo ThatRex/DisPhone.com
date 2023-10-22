c990a32e49e3d32843e9c801abfc721bcbb3cf59
Rex <48845898+ThatRex@users.noreply.github.com>
2023-10-22 23:38:28 +0200

    New features (#14)
    * updated code to be consistent
    
    * Added #12; Added #11

--- src/lib/utils/start-media-flow.ts
+++ src/lib/utils/start-media-flow.ts
@@ -1,12 +1,12 @@
 /** For Chromium Compat. See https://stackoverflow.com/questions/24287054/chrome-wont-play-webaudio-getusermedia-via-webrtc-peer-js/ */
-export function startMediaFlow(trackOrStream: MediaStream | MediaStreamTrack) {
+export function startMediaFlow(track_or_stream: MediaStream | MediaStreamTrack) {
 	let stream: MediaStream
 
-	if (trackOrStream instanceof MediaStreamTrack) {
+	if (track_or_stream instanceof MediaStreamTrack) {
 		stream = new MediaStream()
-		stream.addTrack(trackOrStream)
+		stream.addTrack(track_or_stream)
 	} else {
-		stream = trackOrStream
+		stream = track_or_stream
 	}
 
 	let a: HTMLAudioElement | undefined = new Audio()
