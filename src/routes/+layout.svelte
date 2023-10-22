602188c7232b75624a418f685ce78bcf21b76ae8
Rex <48845898+ThatRex@users.noreply.github.com>
2023-10-22 02:55:42 +0200

    added vercel analytics code (#9)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-<slot/>
+<script lang="ts">
+	import { dev } from '$app/environment'
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' })
+</script>
+
+<slot />
