2101187c392b161ebff916b2a82f4951743b1f95
Rex <48845898+ThatRex@users.noreply.github.com>
2023-12-03 03:56:25 +0200

    Meta Fix; Font Fix; Readme Update (#30)
    * fixed font css
    
    * fixed meta
    
    * updated readme

--- src/lib/components/meta.svelte
+++ src/lib/components/meta.svelte
@@ -2,6 +2,7 @@
 	import { dev } from '$app/environment'
 
 	export let title: string | string[] = ''
+	export let description: string = ''
 
 	$: title_list = ['BaitKit.net', ...(typeof title === 'string' ? [title] : title)].filter((i) => i)
 	$: full_title = `${dev ? '[InDev] ' : ''}${title_list.reverse().join(' • ')}`
@@ -9,5 +10,10 @@
 
 <svelte:head>
 	<title>{full_title}</title>
-	<meta name="title" content="{full_title}" />
+	<meta name="title" content={full_title} />
+	<meta property="og:title" content={full_title} />
+	{#if description}
+		<meta name="description" content={description} />
+		<meta property="og:description" content={description} />
+	{/if}
 </svelte:head>
---
+++ src/routes/disphone/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { browser } from '$app/environment'
+	import Meta from '$lib/components/meta.svelte'
+</script>
+
+<Meta title="DisPhone" description="Browser softphone with Discord integration." />
+
+{#if browser}<slot />{/if}
