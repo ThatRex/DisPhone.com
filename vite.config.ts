162bccf800a604498c9fa709f3d12225e1629f87
Rex <rex@rexslab.com>
2023-12-27 19:25:25 +0200

    migration to SvelteKit 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-	server: {
-		https: true
-	},
+	server: { https: true, proxy: {} },
 	plugins: [sveltekit(), mkcert()]
 })
