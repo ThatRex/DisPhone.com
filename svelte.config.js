ea4ebc7266215e15e43ca160209ff394e59ca5d3
Rex <rex@rexslab.com>
2024-06-23 03:55:12 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { preprocessMeltUI } from '@melt-ui/pp'
 import sequence from 'svelte-sequential-preprocessor'
@@ -10,13 +10,8 @@ const pkg = JSON.parse(readFileSync(path, 'utf8'))
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		version: { name: pkg.version }
 	}
